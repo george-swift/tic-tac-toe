e7f7b445d910f44e31f87856cd8761963e8b1796
George <ubong_george@ymail.com>
2021-02-16 23:01:56 +0100

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/env ruby
 
-# frozen_string_literal: true
-
 # require_relative '../lib/tic_tac_toe'
 
-puts "Player 1: Enter Your Name: !"
+puts 'Player 1: Enter Your Name: !'
 
 player1 = gets.chomp
 
-puts "Player 2: Enter Your Name: !"
+puts 'Player 2: Enter Your Name: !'
 player2 = gets.chomp
 
 puts "welcome: #{player1} & #{player2}. Ready to Battle?"
