05a1313203c5131273e81376ff7a8228398f72c8
smy5152 <shabbiryamani@gmail.com>
2021-02-16 22:17:52 +0200

    Milestone 1: Setup

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,14 @@
+#!/usr/bin/env ruby
+
 # frozen_string_literal: true
 
-require_relative '../lib/tic_tac_toe'
+# require_relative '../lib/tic_tac_toe'
 
 puts "Player 1: Enter Your Name: !"
+
+player_1 = gets.chomp
+
+puts "Player 2: Enter Your Name: !"
+player_2 = gets.chomp
+
+puts "welcome: #{player_1} & #{player_2}. Ready to Battle?"
