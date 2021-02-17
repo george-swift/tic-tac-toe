f93c0909555be1060012bb94617ee2be88f767d5
smy5152 <shabbiryamani@gmail.com>
2021-02-18 01:41:23 +0200

    VALIDATE & ACCEPT cell selection

--- bin/main.rb
+++ bin/main.rb
@@ -6,10 +6,10 @@ require_relative "../lib/tic_tac_toe/player"
 # Use getters to collect input from players
 puts "Let's play tic-tac-toe!"
 puts 'Enter your name player one'
-name = gets.chomp
+name = 'ABC' # gets.chomp
 player_one = name
 puts "Welcome #{player_one}, select either 'X' or 'O' as your marker"
-marker = gets.chomp.upcase
+marker = 'X' # gets.chomp.upcase
 
 # Validate input from the players
 def validate_marker(player_one, marker)
@@ -32,7 +32,7 @@ def change_marker(marker)
 end
 
 puts 'Enter your name player two'
-name = gets.chomp
+name = 'XYZ' # gets.chomp
 player_two = name
 
 # Create Player 2 object
@@ -67,11 +67,16 @@ def accept_moves(player_one, player_two)
   board(cells)
   while cells.any? { |n| n.is_a? Integer }
     puts 'Select a number from the GRID to make your move.'
+    
     puts "#{player_one.name}'s Turn : "
-    move_pos = position_available(gets.chomp.to_i-1, player_one, player_two)    
+    move_pos = gets.chomp.to_i-1
+    while (position_available(move_pos, player_one, player_two)) == false
+      puts "Oops! Invalid input. Try Again."
+      move_pos = gets.chomp.to_i-1
+    end
     # accept player 1 move
     player_one.moves_arr[move_pos] = 1
-    p player_one.moves_arr
+    # p player_one.moves_arr
 
     # reference player_object.marker for DISPLAY GRID
     cells[move_pos] = player_one.marker
@@ -81,14 +86,14 @@ def accept_moves(player_one, player_two)
     unless board_is_full(cells)
       puts 'Select a number from the GRID to make your move.'
       # accept player 2 move
-
-      # if position_available?
       puts "#{player_two.name}'s Turn : "
-      move_pos = position_available(gets.chomp.to_i-1, player_one, player_two)          # end
-      
+      move_pos = gets.chomp.to_i-1
+      while (position_available(move_pos, player_one, player_two)) == false
+        puts "Oops! Invalid input. Try Again."
+        move_pos = gets.chomp.to_i-1
+      end 
       # accept player 2 move
       player_two.moves_arr[move_pos] = 1
-      p player_two.moves_arr
       # reference player_object.marker
       cells[move_pos] = player_two.marker
 
@@ -109,19 +114,16 @@ def board_is_full(cells)
   false
 end
 
-
 # checks moves array for Player 1 & 2
 # allow move position & returns true
 # checks against moves array of both players
 def position_available(move_pos, player_1_Obj, player_2_Obj)
-
-  unless ((player_1_Obj.moves_arr[move_pos] == 0) && (player_2_Obj.moves_arr[move_pos] == 0))
-      puts "Oops1 The cell is taken. Try Again."
+  p "player1:",player_1_Obj.moves_arr
+  p "player2:",player_2_Obj.moves_arr
+  if ((player_1_Obj.moves_arr[move_pos] == 0) && (player_2_Obj.moves_arr[move_pos] == 0)) == true
+          return true
   end
-  
-    move_pos = gets.chomp.to_i-1
-
-  return move_pos
+  return false
 end
 
 accept_moves(player_1_Obj, player_2_Obj)
