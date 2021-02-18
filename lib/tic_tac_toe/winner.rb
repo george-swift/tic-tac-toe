80911f9588ec9fc3067f747aac735ec945db1954
smy5152 <shabbiryamani@gmail.com>
2021-02-18 17:43:55 +0200

    Final Linters/Rubocop fixed

--- lib/tic_tac_toe/winner.rb
+++ lib/tic_tac_toe/winner.rb
@@ -4,6 +4,5 @@ class FindWinner
   def game_over?(arr)
     win = arr.map.with_index { |num, idx| num.odd? ? idx : nil }.compact
     return COMBOS.any? {|combo| (combo & win).size > 2 }
-  end                             [0,1,2] & [0,1,2]
-
+  end
 end
