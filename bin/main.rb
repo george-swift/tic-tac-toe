053631e9b9083f02d950b4ad7b6bd212e9f7f1d8
George <ubong_george@ymail.com>
2021-02-18 22:21:43 +0100

    Disable linters for banner

--- bin/main.rb
+++ bin/main.rb
@@ -61,6 +61,7 @@ def board(moved_cells = %w[1 2 3 4 5 6 7 8 9])
   GRID
 end
 
+# rubocop:disable Lint/LiteralInInterpolation
 # Add the banner
 def game_banner
   puts <<-GRID
@@ -75,6 +76,7 @@ def game_banner
          ###   ###  ####     ###   #####  ####     ###    ####   ####
   GRID
 end
+# rubocop:enable Lint/LiteralInInterpolation
 
 def accept_moves(player_one, player_two)
   cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
