5d9ed76e9659d15485530985004c6f3bb31f494b
George <ubong_george@ymail.com>
2021-02-18 16:45:06 +0100

    Fix linter errors

--- lib/tic_tac_toe/winner.rb
+++ lib/tic_tac_toe/winner.rb
@@ -1,8 +1,8 @@
 class FindWinner
-  COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
 
   def game_over?(arr)
     win = arr.map.with_index { |num, idx| num.odd? ? idx : nil }.compact
-    return COMBOS.any? {|combo| (combo & win).size > 2 }
+    COMBOS.any? { |combo| (combo & win).size > 2 }
   end
 end
