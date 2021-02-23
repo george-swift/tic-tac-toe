503501c168d07bc471d75a5fe8d6688f9a89a333
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-23 13:27:48 -0300

    Add tests for FindWinner class

--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../lib/tic_tac_toe/winner.rb'
+
+describe FindWinner do
+  marker = %w(X O)
+  let(:arr_winner) { [1, 1, 1, 0, 0, 0, 0, 0, 0] }
+  let(:arr_loser) { [1, 0, 1, 0, 0, 0, 0, 0, 0] }
+
+  describe '#game_over?' do
+    context 'when a player makes a move it checks for a winning combination' do
+      it "returns true if the player's moves equal one of the winning combinations" do
+        expect(FindWinner.new.game_over?(arr_winner)).to be_truthy
+      end
+      it "doesn´t return true if the player's moves are not one of the winning combinations" do
+        expect(FindWinner.new.game_over?(arr_loser)).not_to be_truthy
+      end
+    end
+  end
+
+end
